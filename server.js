42b5b2621950d3ebacb902e1561c1687c15a7e01
Erica P <erica.parra@rocketmail.com>
2019-07-04 11:49:19 -0700

    server set up to mongo via mongoose

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const express = require('express')
-
 const app = express()
 
-app.listen(3001)
+//Middleware
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+//Routes 
+require('./routes')(app)
+
+//mongoose connection 
+require('mongoose').connect('mongodb://localhost/googlebooks_db', { useNewUrlParser : true , useCreateIndex: true, useFindAndModify: true})
+    .then(_ =>app.listen(3001))
+    .catch(err => console.log(err))
+
